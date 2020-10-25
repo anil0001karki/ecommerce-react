a916e926200fbec49932f5f9b5b14fa12efd0bd9
anil karki <anilkarki00001@gmail.com>
2020-10-25 05:13:51 -0700

    add-redux-filter

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,9 +1,22 @@
-import { FETCT_PRODUCTS } from "../type";
+import { FETCT_PRODUCTS, FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } from "../type";
 
 export const productReducer=(state={},action)=>{
     switch(action.type){
-        case FETCT_PRODUCTS:
-            return {items:action.payload};
+        case FILTER_PRODUCTS_BY_SIZE:
+          
+            return {
+              ...state,
+              size: action.payload.size,
+              filteredItems: action.payload.items,
+            };
+            case ORDER_PRODUCTS_BY_PRICE:
+                return {
+                  ...state,
+                  sort: action.payload.sort,
+                  filteredItems: action.payload.items,
+                };
+                case  FETCT_PRODUCTS:
+                    return { items: action.payload, filteredItems: action.payload };
         default:
             return state;
     }
